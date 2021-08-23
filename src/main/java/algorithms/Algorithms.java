932a009b1cfbf895d805ef25115db0cc18fe2de0
baszabo <balazs.szabo@ni.com>
2021-08-23 13:54:18 +0200

    implemented factorial function

--- src/main/java/algorithms/Algorithms.java
+++ src/main/java/algorithms/Algorithms.java
@@ -28,4 +28,11 @@ public class Algorithms {
         return reversedWord.toString();
     }
 
+    public static long factorial(int number) {
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+        return number * factorial(number-1);
+    }
+
 }
